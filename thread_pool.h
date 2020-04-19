5d5bf0210a1855faf8858b37723b772e2b55e3cb
Squanchy <43287918+YiyongHuang@users.noreply.github.com>
2020-04-19 17:16:04 +0800

    Update thread_pool.h

--- thread_pool.h
+++ thread_pool.h
@@ -7,21 +7,19 @@
 #include "http_request.h"
 #include "epoll.h"
 
-const int THREADPOOL_INVALID = -1;
-const int THREADPOOL_LOCK_FAILURE = -2;
-const int THREADPOOL_QUEUE_FULL = -3;
-const int THREADPOOL_SHUTDOWN = -4;
-const int THREADPOOL_THREAD_FAILURE = -5;
-const int THREADPOOL_SIGNAL_FAILURE = -6;
-const int THREADPOOL_GRACEFUL = 1;
+#define THREADPOOL_SIGNAL_FAILURE -1
+#define THREADPOOL_LOCK_FAILURE -2
+#define THREADPOOL_QUEUE_FULL -3
+#define THREADPOOL_SHUTDOWN -4
+#define THREADPOOL_THREAD_FAILURE -5
 
-const int MAX_THREADS = 1024;
-const int MAX_QUEUE = 65535;
+#define MAX_THREADS 1024
+#define MAX_QUEUE 65535
 
 typedef enum
 {
-    immediate_shutdown = 1,
-    graceful_shutdown = 2
+    IMMEDIATE_SHUTDOWN = 1,
+    GRACEFUL_SHUTDOWN = 2
 } ShutDownOption;
 
 struct ThreadPoolTask
@@ -34,23 +32,23 @@ struct ThreadPoolTask
 class ThreadPool
 {
 private:
-    static pthread_mutex_t lock;
-    static pthread_cond_t notify;
+    static pthread_mutex_t lock_;
+    static pthread_cond_t notify_;
 
-    static std::vector<pthread_t> threads;
-    static std::vector<ThreadPoolTask> queue;
-    static int thread_count;
-    static int queue_size;
-    static int head;
-    static int tail;
-    static int count; // 任务队列当前任务数量
-    static int shutdown;
-    static int started;
+    static std::vector<pthread_t> threads_;
+    static std::vector<ThreadPoolTask> queue_;
+    static int thread_count_;
+    static int queue_size_;
+    static int head_;
+    static int tail_;
+    static int count_; // 任务队列当前任务数量
+    static int shutdown_;
+    static int started_;
 
 public:
-    static int threadpoolCreate(int _thread_count, int _queue_size);
+    static int threadpoolCreate(int thread_count, int queue_size);
     static int threadpoolAdd(std::shared_ptr<void> args, std::function<void(std::shared_ptr<void>)> fun);
-    static int threadpoolDestroy(ShutDownOption shutdown_option = graceful_shutdown);
+    static int threadpoolDestroy(ShutDownOption shutdown_option = GRACEFUL_SHUTDOWN);
     static int threadpoolFree();
     static void *threadpoolWorker(void *args);
 };
